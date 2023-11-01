91c0544a260813f2caf6dee2b9c7f24acdbdd992
ratnov22 <antonratnov@yandex.ru>
2023-11-01 03:19:48 +0300

    add func date

---
+++ app/components/screens/calendar/Calendar.tsx
@@ -0,0 +1,7 @@
+import { Text, View } from "react-native"
+
+export const Calendar = () => {
+    return <View>
+        <Text>ewfefwfewwe</Text>
+    </View>
+}
--- app/components/screens/profile/calendar-min/CalendarMin.tsx
+++ app/components/screens/profile/calendar-min/CalendarMin.tsx
@@ -3,13 +3,25 @@ import { useQuery } from '@tanstack/react-query'
 import { FC } from 'react'
 import { Image, Pressable, Text, View } from 'react-native'
 import clsx from 'clsx'
-
+declare global {
+	interface Date {
+		daysInMonth(): number
+	}
+} //@TASK
 export const CalendarMin: FC = () => {
 	const minDate = new Date()
-	const arrayDate = Array.from(
-		{ length: 14 },
-		(_, i) => -minDate.getDate() + i - 1
-	)
+	const addDate = new Date()
+	let month = minDate.getMonth()
+
+	Date.prototype.daysInMonth = function () {
+		return 33 - new Date(this.getFullYear(), this.getMonth(), 33).getDate()
+	}
+	addDate.setDate(addDate.getDate() - 7)
+	const arrayDate = Array.from({ length: 14 }, (_, i) => {
+		addDate.setDate(addDate.getDate() + 1)
+		let curDate = addDate.getDate()
+		return curDate
+	})
 	return (
 		<View className='bg-gray-700 rounded-xl p-4 mt-5'>
 			<Text className='text-white mb-4 text-2xl font-bold'>Last 14 days</Text>
@@ -34,11 +46,11 @@ export const CalendarMin: FC = () => {
 					</View>
 				))}
 			</View>
-            <View>
-                <Pressable className='p-3 mx-auto my-5 rounded-2xl border-2 border-white' >
-                    <Text className='text-white text-xl'>View all my memories</Text>
-                </Pressable>
-            </View>
+			<View>
+				<Pressable className='p-3 mx-auto my-5 rounded-2xl border-2 border-white'>
+					<Text className='text-white text-xl'>View all my memories</Text>
+				</Pressable>
+			</View>
 		</View>
 	)
 }
