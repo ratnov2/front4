2a681f7c7f5f14285a8e9d62f2ada4c69377fd53
ratnov22 <antonratnov@yandex.ru>
2024-02-07 23:44:50 +0300

    add dissmiss in textinput

--- app/components/screens/friends/Friends.tsx
+++ app/components/screens/friends/Friends.tsx
@@ -1,6 +1,7 @@
 import { useAuth } from '@/hooks/useAuth'
 import { FriendsService } from '@/services/friends/friends.service'
 import { Link, useNavigation, useRoute } from '@react-navigation/native'
+
 import {
 	QueryCache,
 	useMutation,
@@ -12,6 +13,8 @@ import cn from 'clsx'
 import {
 	ActivityIndicator,
 	Image,
+	Keyboard,
+	KeyboardAvoidingView,
 	Modal,
 	Pressable,
 	ScrollView,
@@ -19,6 +22,7 @@ import {
 	TextInput,
 	Touchable,
 	TouchableOpacity,
+	TouchableWithoutFeedback,
 	View
 } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
@@ -38,6 +42,8 @@ import BottomDrawer, {
 } from 'react-native-animated-bottom-drawer'
 import { FriendItem } from './ui/friend-item'
 import { useSearchingFriends } from './useSearchingFriends'
+import { Entypo, EvilIcons } from '@expo/vector-icons'
+import DismissKeyboard from '@/ui/form-elements/field/DismissKeyboard'
 
 export const Friends = () => {
 	const insets = useSafeAreaInsets()
@@ -73,7 +79,7 @@ export const Friends = () => {
 
 	const [typeFriend, setTypeFriend] = useState<
 		'suggestion' | 'friends' | 'requests'
-	>('requests')
+	>('suggestion')
 	// useEffect(() => {
 	// 	if (!myFriends.isLoading) {
 	// 		myFriends.refetch()
@@ -97,8 +103,6 @@ export const Friends = () => {
 		friendId: string
 		status: '0' | '1' | '2' | '3'
 	}) => {
-		console.log('!!!')
-
 		if (!addFriend.isLoading) {
 			addFriend.mutate(data)
 		}
@@ -117,206 +121,236 @@ export const Friends = () => {
 	//console.log(getUserByName.data)
 	const ref: any = useRef()
 	const [modalVisible, setModalVisible] = useState(false)
-
 	//const query = currentData.
 	//console.log('!!!', query)
+	const [isStartShouldSetResponder, setIsStartShouldSetResponder] =
+		useState(true)
 	return (
-		<View className='flex-1 mx-4 '>
-			<Text
-				className='text-white text-2xl font-bold text-center left-0 right-0 z-40'
-				style={{ top: insets.top, position: 'absolute' }}
-			>
-				BePrime
-			</Text>
-			<ScrollView className='mb-14'>
-				<View className='relative z-20' style={{ marginTop: insets.top + 50 }}>
-					<View className='flex-row w-full '>
-						<TextInput
-							className='bg-zinc-800 p-4 rounded-2xl text-white flex-1'
-							placeholder='Add or search friends'
-							value={value}
-							onChangeText={e => handlerChangeText(e)}
-						/>
-						{value && (
-							<TouchableOpacity
-								className='items-center flex justify-center mx-3 text-bold'
-								onPress={() => setValue('')}
-							>
-								<Text className='text-white'>Cancel</Text>
-							</TouchableOpacity>
-						)}
-					</View>
-					{/* <Modal
-						animationType='slide'
-						transparent={true}
-						visible={modalVisible}
-					></Modal> */}
-					{/* && getUserByName.data.length > 0 && */}
-				</View>
-				{getUserByName.data || value ? (
-					<View className='flex-1 w-full z-[20] rounded-xl p-4 mt-6' ref={ref}>
-						{getUserByName?.data?.map(e => (
-							<Pressable
-								className='flex-1 '
-								onPress={() =>
-									navigate.navigate({
-										name: `Profile`,
-										params: {
-											id: ''
-										}
-									} as never)
-								}
-								key={e._id}
-							>
-								<FriendItem
-									styles={'mb-4 p-0 bg-transparent flex-1'}
-									name={e.firstName}
-									avatar={e.avatar}
-									buttons={
-										e.friendship[0]?.status === '1' ? (
-											<HandleFriendButton
-												title='Request is sended'
-												id={e._id}
-												status='1'
-											/>
-										) : e.friendship[0]?.status === '2' ? (
-											<HandleFriendButton
-												title='Take friend'
-												id={e._id}
-												status='1'
-											/>
-										) : e.friendship[0]?.status === '3' ? (
-											<HandleFriendButton
-												title='My friend'
-												id={e._id}
-												status='1'
-											/>
-										) : e._id === user?._id ? (
-											<View></View>
-										) : (
-											<HandleFriendButton
-												title='Add friend'
-												loading={addFriend.isLoading}
-												id={e._id}
-												status='1'
-											/>
-										)
-									}
-									body={<FriendBody name={e.firstName} number='' />}
-								/>
-							</Pressable>
-							// <View>
-							// 	<Text className='text-white'>{e._id}</Text>
-							// </View>
-						))}
-						{getUserByName?.data?.length === 0 && (
-							<View className='bg-zinc-600 p-10 rounded-2xl'>
-								<Text className='text-white font-bold'>
-									По вашему запросу ничего не найдено
-								</Text>
-							</View>
-						)}
-						{isDebouncing && (
-							<View className='absolute top-0 w-full '>
-								<Loader />
-							</View>
-						)}
-					</View>
-				) : (
-					<View>
-						<Invite />
-						{typeFriend === 'suggestion' && <SuggestionFriends />}
-						{typeFriend === 'friends' && <MyFriends friends={myFriends} />}
-						{typeFriend === 'requests' && (
-							<RequestFriends friends={myFriends} />
-						)}
-						{typeFriend === 'suggestion' && (
-							<View>
-								<Text>//////</Text>
-								<Text className='text-white'>Add friends</Text>
-								{myFriends.data && (
-									//GET ADD FRIENDS
-									<View>
-										{myFriends.data.friendship.map(friend => {
-											if (friend.status === '2')
-												return (
-													<View className='flex-row  justify-between items-center'>
-														<View>
-															<View className='flex-row'>
-																{friend.friends.avatar ? (
-																	<Image className='w-14 h-14' source={img} />
-																) : (
-																	<View className='bg-red-600 h-14 w-14 rounded-full items-center justify-center'>
-																		<Text className='text-white text-xl font-bold'>
-																			{friend.friends.firstName[0] || 'A'}
-																		</Text>
-																	</View>
-																)}
+		<DismissKeyboard>
+			<View className='flex-1'>
+				<View className='flex-1 '>
+					<Text
+						className='text-white text-2xl font-bold text-center left-0 right-0 z-40'
+						style={{ top: insets.top, position: 'absolute' }}
+					>
+						BePrime
+					</Text>
+					<View style={{ flex: 1}}>
+						<ScrollView style={{ flex: 1, marginBottom: 0, paddingLeft: 14,paddingRight: 14 }}>
+							<View onStartShouldSetResponder={() => true}>
+								<View
+									className='relative z-20'
+									style={{ marginTop: insets.top + 50 }}
+								>
+									<View className='flex-row w-full relative'>
+										<TextInput
+											className='bg-zinc-800 p-2 pl-10 rounded-2xl text-white flex-1 text-xl'
+											placeholder='Add or search friends'
+											placeholderTextColor='rgba(196, 196, 196, 0.45)'
+											value={value}
+											onChangeText={e => handlerChangeText(e)}
+										/>
+										<EvilIcons
+											name='search'
+											size={30}
+											style={{
+												position: 'absolute',
+												left: 6,
+												top: 12,
+												color: 'rgba(196, 196, 196, 0.45)'
+											}}
+										/>
 
-																<Text className='text-white'>
-																	{friend.friends.email}
-																</Text>
-															</View>
-														</View>
-														<TouchableOpacity
-															className='bg-gray-800 p-2 rounded-full h-8 '
-															onPress={() =>
-																handleAddFriend({
-																	friendId: friend.friends._id,
-																	status: '3'
-																})
-															}
-														>
-															<Text className='text-white font-bold'>
-																INVITE
-															</Text>
-															{/* { <ActivityIndicator size="small" color="#0000ff" />} */}
-														</TouchableOpacity>
+										{value && (
+											<TouchableOpacity
+												className='items-center flex justify-center mx-3 text-bold'
+												onPress={() => {
+													setValue('')
+													Keyboard.dismiss()
+												}}
+											>
+												<Text className='text-white'>Cancel</Text>
+											</TouchableOpacity>
+										)}
+									</View>
+								</View>
+								{getUserByName.data || value ? (
+									<View
+										className='flex-1 w-full z-[20] rounded-xl p-4 mt-6'
+										ref={ref}
+									>
+										{getUserByName?.data?.map(e => (
+											<Pressable
+												className='flex-1 '
+												onPress={() =>
+													navigate.navigate({
+														name: `Profile`,
+														params: {
+															id: ''
+														}
+													} as never)
+												}
+												key={e._id}
+											>
+												<FriendItem
+													styles={'mb-4 p-0 bg-transparent flex-1'}
+													name={e.firstName}
+													avatar={e.avatar}
+													buttons={
+														e.friendship[0]?.status === '1' ? (
+															<HandleFriendButton
+																title='Request is sended'
+																id={e._id}
+																status='1'
+															/>
+														) : e.friendship[0]?.status === '2' ? (
+															<HandleFriendButton
+																title='Take friend'
+																id={e._id}
+																status='1'
+															/>
+														) : e.friendship[0]?.status === '3' ? (
+															<HandleFriendButton
+																title='My friend'
+																id={e._id}
+																status='1'
+															/>
+														) : e._id === user?._id ? (
+															<View></View>
+														) : (
+															<HandleFriendButton
+																title='Add friend'
+																loading={addFriend.isLoading}
+																id={e._id}
+																status='1'
+															/>
+														)
+													}
+													body={<FriendBody name={e.firstName} number='' />}
+												/>
+											</Pressable>
+											// <View>
+											// 	<Text className='text-white'>{e._id}</Text>
+											// </View>
+										))}
+										{getUserByName?.data?.length === 0 && (
+											<View className='bg-zinc-600 p-10 rounded-2xl'>
+												<Text className='text-white font-bold'>
+													По вашему запросу ничего не найдено
+												</Text>
+											</View>
+										)}
+										{isDebouncing && (
+											<View className='absolute top-0 w-full '>
+												<Loader />
+											</View>
+										)}
+									</View>
+								) : (
+									<View>
+										<Invite />
+										{typeFriend === 'suggestion' && <SuggestionFriends />}
+										{typeFriend === 'friends' && (
+											<MyFriends friends={myFriends} />
+										)}
+										{typeFriend === 'requests' && (
+											<RequestFriends friends={myFriends} />
+										)}
+										{typeFriend === 'suggestion' && (
+											<View>
+												<Text>//////</Text>
+												<Text className='text-white'>Add friends</Text>
+												{myFriends.data && (
+													//GET ADD FRIENDS
+													<View>
+														{myFriends.data.friendship.map(friend => {
+															if (friend.status === '2')
+																return (
+																	<View className='flex-row  justify-between items-center'>
+																		<View>
+																			<View className='flex-row'>
+																				{friend.friends.avatar ? (
+																					<Image
+																						className='w-14 h-14'
+																						source={img}
+																					/>
+																				) : (
+																					<View className='bg-red-600 h-14 w-14 rounded-full items-center justify-center'>
+																						<Text className='text-white text-xl font-bold'>
+																							{friend.friends.firstName[0] ||
+																								'A'}
+																						</Text>
+																					</View>
+																				)}
+
+																				<Text className='text-white'>
+																					{friend.friends.email}
+																				</Text>
+																			</View>
+																		</View>
+																		<TouchableOpacity
+																			className='bg-gray-800 p-2 rounded-full h-8 '
+																			onPress={() =>
+																				handleAddFriend({
+																					friendId: friend.friends._id,
+																					status: '3'
+																				})
+																			}
+																		>
+																			<Text className='text-white font-bold'>
+																				INVITE
+																			</Text>
+																			{/* { <ActivityIndicator size="small" color="#0000ff" />} */}
+																		</TouchableOpacity>
+																	</View>
+																)
+														})}
 													</View>
-												)
-										})}
+												)}
+											</View>
+										)}
 									</View>
 								)}
 							</View>
-						)}
+						</ScrollView>
 					</View>
-				)}
-			</ScrollView>
-			<View
-				className='bg-slate-800 rounded-full p-2 flex-row justify-between items-center z-40'
-				style={{
-					alignSelf: 'center',
-					position: 'absolute',
-					bottom: insets.bottom
-				}}
-			>
-				<TouchableOpacity
-					className={`${
-						typeFriend === 'suggestion' && 'bg-slate-500'
-					} p-2 rounded-full`}
-					onPress={() => setTypeFriend('suggestion')}
-				>
-					<Text className='text-white'>Suggestion</Text>
-				</TouchableOpacity>
+					<View
+						className='bg-slate-800 rounded-full p-2 flex-row justify-between items-center z-40'
+						style={{
+							alignSelf: 'center',
+							position: 'absolute',
+							bottom: insets.bottom
+						}}
+					>
+						<TouchableOpacity
+							className={`${
+								typeFriend === 'suggestion' && 'bg-slate-500'
+							} p-2 rounded-full`}
+							onPress={() => setTypeFriend('suggestion')}
+						>
+							<Text className='text-white'>Suggestion</Text>
+						</TouchableOpacity>
 
-				<TouchableOpacity
-					className={`${
-						typeFriend === 'friends' && 'bg-slate-500'
-					} p-2 rounded-full ml-1`}
-					onPress={() => setTypeFriend('friends')}
-				>
-					<Text className='text-white'>Friends</Text>
-				</TouchableOpacity>
-				<TouchableOpacity
-					className={`${
-						typeFriend === 'requests' && 'bg-slate-500'
-					} p-2 rounded-full ml-1`}
-					onPress={() => setTypeFriend('requests')}
-				>
-					<Text className='text-white'>Requests</Text>
-				</TouchableOpacity>
+						<TouchableOpacity
+							className={`${
+								typeFriend === 'friends' && 'bg-slate-500'
+							} p-2 rounded-full ml-1`}
+							onPress={() => setTypeFriend('friends')}
+						>
+							<Text className='text-white'>Friends</Text>
+						</TouchableOpacity>
+						<TouchableOpacity
+							className={`${
+								typeFriend === 'requests' && 'bg-slate-500'
+							} p-2 rounded-full ml-1`}
+							onPress={() => setTypeFriend('requests')}
+						>
+							<Text className='text-white'>Requests</Text>
+						</TouchableOpacity>
+					</View>
+				</View>
 			</View>
-		</View>
+		</DismissKeyboard>
 	)
 }
 
@@ -405,7 +439,9 @@ const HandleFriendButton = ({
 					}
 
 					//prevData = queryClient.getQueryData(['get-user-by-name'])
-					const updatedIndex = prevData.findIndex((item: { _id: any }) => item._id === e._id)
+					const updatedIndex = prevData.findIndex(
+						(item: { _id: any }) => item._id === e._id
+					)
 					//currentData.map((e: any)=>)
 					if (updatedIndex !== -1) {
 						// Создайте новый массив данных с обновленной строкой
